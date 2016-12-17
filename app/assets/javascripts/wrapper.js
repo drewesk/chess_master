30cd606f7d06b2fa942a17f08aadd9e72a921c13
Andrew Eskenazi <Drewesk@gmail.com>
2016-12-17 03:28:05 -0700

    Added youtube video functionality

---
+++ app/assets/javascripts/wrapper.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'xmXwdoRG43U', start: 300 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
