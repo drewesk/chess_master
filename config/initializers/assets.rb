418042de41fbd52d8e723d77a971d23b2bfda989
Andrew Eskenazi <Drewesk@gmail.com>
2016-12-17 22:56:55 -0700

    CSS and API integrations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-Rails.application.config.assets.precompile += %w( jquery.tubular.1.0.js wrapper.js )
+Rails.application.config.assets.precompile += %w( jquery.tubular.1.0.js wrapper.js chessboard.js )
+Rails.application.config.assets.precompile += %w( chessboard.css )
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
