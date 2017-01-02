ab7ec16338d05e205016fcb19c7f5bb9a295870e
Andrew Eskenazi <Drewesk@gmail.com>
2017-01-01 21:23:20 -0700

    Added ability to change the Background
    * Added a prompt to wrapper.js
    
    * Condensed and Refactored the ‘Hide/Show’ Button

--- app/assets/javascripts/wrapper.js
+++ app/assets/javascripts/wrapper.js
@@ -1,6 +1,51 @@
 $('document').ready(function() {
-  var videoDefault = 'xmXwdoRG43U';
+  var videoDefault;
 
+  videoDefault = prompt('Add a YouTube ID. The Prompt may pop up a second time due to Maintenance.');
+  if (videoDefault == ''){
+    videoDefault = 'xmXwdoRG43U';
+  }
   var options = { videoId: videoDefault, start: 300 };
   $('#wrapper').tubular(options);
+
 });
+
+
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your list is empty!');
+    } else {
+      console.log('My Todos:')
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
+
