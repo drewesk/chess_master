eae85785aba474d69b87b192ee5b3548041de343
Andrew Eskenazi <Drewesk@gmail.com>
2017-01-03 20:22:10 -0700

    Edits to Tubular API and hide feature
    * divs with id `hide_content_top`and `hide_content_bottom`.
    
    * JQuery toggle hide and show for each div.
    
    * Changed the default screen ratio for the video.

--- app/assets/javascripts/jquery.tubular.1.0.js
+++ app/assets/javascripts/jquery.tubular.1.0.js
@@ -16,7 +16,7 @@
     
     // defaults
     var defaults = {
-        ratio: 16/9, // usually either 4/3 or 16/9 -- tweak as needed
+        ratio: 16/6.5, // usually either 4/3 or 16/9 -- tweak as needed
         videoId: 'ZCAnLxRvNNc', // toy robot in space is a good default, no?
         mute: true,
         repeat: true,
