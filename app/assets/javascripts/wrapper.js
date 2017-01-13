fd9512c02c86d8ebd774c4e01e97cd7cb75876a9
Andrew Eskenazi <Drewesk@gmail.com>
2017-01-13 00:22:22 -0700

    Improvements on setting background on-demand
    * wrapper.js is now dependent on click event to set background.

--- app/assets/javascripts/wrapper.js
+++ app/assets/javascripts/wrapper.js
@@ -1,36 +1,37 @@
 $('document').ready(function() {
-  var videoDefault;
-  var time;
+  $('#set_background').click(function() {
+    var videoDefault;
+    var time;
 
-  videoDefault = prompt('Enter YouTube ID,\n' +
-    'ID example( ZCAnLxRvNNc ),\n' +
-    'Or type( lesson of the week ),\n' +
-    'Default Video if  left empty.\n' +
-    'none = no Video');
+    videoDefault = prompt('Enter YouTube ID,\n' +
+      'ID example( ZCAnLxRvNNc ),\n' +
+      'Or type( lesson of the week ),\n' +
+      'Default Video if  left empty.\n' +
+      'none = no Video');
 
-  switch (videoDefault) {
-    case 'lesson of the week':
-      videoDefault = 'JuLYgClRe-Q';
-      time = 0;
-      break;
-    case  '':
-      videoDefault = 'xmXwdoRG43U';
-      time = 300;
-      break;
-    case  null:
-      videoDefault = 'xmXwdoRG43U';
-      time = 300;
-      break;
-    default:
-      time = 0;
-      break;
-  }
+    switch (videoDefault) {
+      case 'lesson of the week':
+        videoDefault = 'JuLYgClRe-Q';
+        time = 0;
+        break;
+      case  '':
+        videoDefault = 'xmXwdoRG43U';
+        time = 300;
+        break;
+      case  null:
+        videoDefault = 'xmXwdoRG43U';
+        time = 300;
+        break;
+      default:
+        time = 0;
+        break;
+    }
 
-  if (videoDefault === 'none') {
-    // do nothing
-  } else {
-    var options = {videoId: videoDefault, start: time};
-    $('#wrapper').tubular(options);
-  }
-
+    if (videoDefault === 'none') {
+      // do nothing
+    } else {
+      var options = {videoId: videoDefault, start: time};
+      $('#wrapper').tubular(options);
+    }
+  });
 });
